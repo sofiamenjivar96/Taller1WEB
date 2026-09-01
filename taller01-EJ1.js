cd3d789f705b4c4b04e6cea8199abf90fdf97d00
sofiamenjivar96 <00074525@uca.edu.sv>
2026-09-01 16:33:57 -0600

    feat: Agregar Ejercicio 1

---
+++ taller01-EJ1.js
@@ -0,0 +1,17 @@
+
+// EJERCICIO 1
+
+function CalcularDescuento (nombreProducto, precioProducto, porcentajeDescuento){
+
+    if(precioProducto>=0 && porcentajeDescuento>=0){ //validar que los datos sean positivos
+        let descuento = precioProducto * (porcentajeDescuento/100);
+        let precioFinal = precioProducto - descuento;
+        return nombreProducto + " ,Precio final: " + precioFinal;
+    } else {
+        return "Datos invalidos, Ingrese valores positivos";
+    }
+}
+
+console.log(CalcularDescuento("Bocina",100,30));
+console.log(calcularDescuento("Camisa", 50, 20));
+console.log(calcularDescuento("Cartera", -10, -30));
